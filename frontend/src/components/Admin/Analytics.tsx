8ac299ebd060ab6075922ab57da34b73623c51fe
Sujit <mnamesujit@gmail.com>
2023-10-28 18:33:07 +0530

    admin dashboard ui with sample data

--- frontend/src/components/Admin/Analytics.tsx
+++ frontend/src/components/Admin/Analytics.tsx
@@ -1,7 +1,52 @@
-import { Typography } from '@mui/material';
+
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+// Sample data
+const data = [
+  { name: 'Student 1', branch: 'Computer Science', course: 'B.Tech' },
+  { name: 'Student 2', branch: 'Electrical Engineering', course: 'B.Tech' },
+  { name: 'Student 3', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 1', branch: 'Computer Science', course: 'B.Tech' },
+  { name: 'Student 2', branch: 'Electrical Engineering', course: 'B.Tech' },
+  { name: 'Student 3', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 1', branch: 'Computer Science', course: 'B.Tech' },
+  { name: 'Student 2', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 3', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 1', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 2', branch: 'Mechanical Engineering', course: 'B.Tech' },
+  { name: 'Student 3', branch: 'Civil Engineering', course: 'B.Tech' },
+];
 
 const Analytics: React.FC = () => {
-    return <Typography variant="h4">Analytics page</Typography>;
-}
+  const branchData = data.reduce((acc, entry) => {
+    acc[entry.branch] = (acc[entry.branch] || 0) + 1;
+    return acc;
+  }, {} as Record<string, number>);
+
+  const chartData = Object.keys(branchData).map((branch) => ({
+    branch,
+    students: branchData[branch],
+  }));
+
+  return (
+    <div>
+      <h2>Student Analytics</h2>
+      <ResponsiveContainer width={700} height={400}>
+        <BarChart
+          data={chartData}
+          margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="branch" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="students" fill="#8884d8" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
 export default Analytics;
--- frontend/src/components/Admin/Data.tsx
+++ frontend/src/components/Admin/Data.tsx
@@ -1,10 +1,72 @@
+import { useState, useEffect } from 'react';
+import { Table, TableContainer, TableHead, TableRow, TableCell, TableBody, Paper, Typography, CircularProgress, styled } from '@mui/material';
+
+const LoadingContainer = styled('div')({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '200px',
+});
+
+interface Student {
+  id: number;
+  name: string;
+  email: string;
+  branch: string;
+}
+
+const dummyData: Student[] = [
+  { id: 1, name: 'John Doe', email: 'john@example.com', branch: 'Computer Science' },
+  { id: 2, name: 'Jane Doe', email: 'jane@example.com', branch: 'Electrical Engineering' },
+];
 
 const Data: React.FC = () => {
-  
+  const [students, setStudents] = useState<Student[]>(dummyData);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+   
+    setLoading(true);
+    setTimeout(() => {
+      setStudents(dummyData);
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div>
-      <h1>Data Page</h1>
-    </div>
+    <>
+      <Typography variant="h4" gutterBottom>
+        Student Details:
+      </Typography>
+      {loading ? (
+        <LoadingContainer>
+          <CircularProgress />
+        </LoadingContainer>
+      ) : (
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Branch</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {students.map((student) => (
+                <TableRow key={student.id}>
+                  <TableCell>{student.id}</TableCell>
+                  <TableCell>{student.name}</TableCell>
+                  <TableCell>{student.email}</TableCell>
+                  <TableCell>{student.branch}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </>
   );
 };
 
